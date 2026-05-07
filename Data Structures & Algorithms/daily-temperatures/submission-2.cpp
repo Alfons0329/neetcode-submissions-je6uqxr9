55ad55f02af31f90858d21c10b9475b24a31b618
Alfons Hwu <alfons.cs04@g2.nctu.edu.tw>
2026-05-07 11:10:58 +0800

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.cpp
@@ -0,0 +1,45 @@
+
+/*
+Intuition: Since to track the happened before now (backtrack, revisit) we cna use stack
+
+Approach:
+    1. init res with 0
+    2. st trace <temp, pos>
+    3. for temp[idx] temps
+        if stack non empty, and temp > st.top()
+            res[st.top().pos] = idx - st.top().pos
+        
+        st.push(<temp[idx], idx>)
+
+Complexity:
+    Time: O(temps.size()) at most [n, n-1, n-2.....0] we push all the temps to stack, O(1) for push every elemtn, total O(temps.sizze())
+    Space: O(temps.size()) same as above
+
+*/
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temps) {
+        int n = temps.size();
+        vector<int> res(n, 0);
+        stack<pair<int, int>> st;
+
+        for (int i = 0; i < n; i++) {
+            int cur = temps[i];
+
+            if (!st.empty() && st.top().first < cur) {
+                while(!st.empty()) {
+                    if (st.top().first < cur) {
+                        // count days duration delta, how many days after can reach higher
+                        res[st.top().second] = i - st.top().second;
+                        st.pop();
+                    } else {
+                        break;
+                    }
+                }
+            }
+            st.push({cur, i});
+        }
+
+        return res;
+    }
+};
