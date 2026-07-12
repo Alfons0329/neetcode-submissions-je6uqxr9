17c5df38729f02ed9b17069ec4a0a2541226e137
Alfons Hwu <alfons.cs04@g2.nctu.edu.tw>
2026-07-12 12:03:25 +0800

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.go
@@ -0,0 +1,15 @@
+func majorityElement(nums []int) int {
+	freq := make(map[int]int)
+
+	for _, num := range(nums) {
+		freq[num]++
+	}
+
+	for k, v := range(freq) {
+		if v > int(len(nums) / 2) {
+			return k
+		}
+	}
+
+	return -1
+}
