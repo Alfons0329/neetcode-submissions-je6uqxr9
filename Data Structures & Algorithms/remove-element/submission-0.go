7c618d1df772a4471fa8ca6e60acd77417d078db
Alfons Hwu <alfons.cs04@g2.nctu.edu.tw>
2026-07-15 10:00:08 +0800

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.go
@@ -0,0 +1,11 @@
+func removeElement(nums []int, val int) int {
+    remainIdx := 0
+    for i := 0; i < len(nums); i++ {
+        if nums[i] != val {
+            nums[remainIdx] = nums[i]
+            remainIdx++
+        }
+    }
+
+    return remainIdx
+}
